8c44da5e12f51a2baa6e3fabb959f6b3aec0620b
Imran Mirza <imranmirza070901@gmail.com>
2024-04-08 14:11:24 +0530

    provacy and terms fixes

--- src/utils/permalinks.ts
+++ src/utils/permalinks.ts
@@ -25,9 +25,20 @@ export const BLOG_BASE = cleanSlug(APP_BLOG?.list?.pathname);
 export const CATEGORY_BASE = cleanSlug(APP_BLOG?.category?.pathname);
 export const TAG_BASE = cleanSlug(APP_BLOG?.tag?.pathname) || 'tag';
 
-export const POST_PERMALINK_PATTERN = trimSlash(APP_BLOG?.post?.permalink || `${BLOG_BASE}/%slug%`);
-
 /** */
+export const getPermalink = (slug = '', type = 'page'): string => {
+  let permalink: string;
+
+  switch (type) {
+    case 'page':
+    default:
+      permalink = createPath(slug);
+      break;
+  }
+
+  return definitivePermalink(permalink);
+};
+
 export const getCanonical = (path = ''): string | URL => {
   const url = String(new URL(path, SITE.site));
   if (SITE.trailingSlash == false && path && url.endsWith('/')) {
@@ -36,32 +47,6 @@ export const getCanonical = (path = ''): string | URL => {
     return url + '/';
   }
   return url;
-};
-
-/** */
-export const getPermalink = (slug = '', type = 'page'): string => {
-  let permalink: string;
-
-  switch (type) {
-    case 'category':
-      permalink = createPath(CATEGORY_BASE, trimSlash(slug));
-      break;
-
-    case 'tag':
-      permalink = createPath(TAG_BASE, trimSlash(slug));
-      break;
-
-    case 'post':
-      permalink = createPath(trimSlash(slug));
-      break;
-
-    case 'page':
-    default:
-      permalink = createPath(slug);
-      break;
-  }
-
-  return definitivePermalink(permalink);
 };
 
 /** */
